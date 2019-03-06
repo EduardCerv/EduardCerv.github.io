831d79c7f13e0d6afffccc45742028a2da77a5c6
Eduardo Cervantes <eduardo_c_j@live.com.mx>
2019-03-05 23:28:51 -0600

    Add new template for cv

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
-(function (main) {
-    main(window.jQuery, window, document);
-}(function ($, window, document) {
-    // global variables for this javascript
-    var app = {
-        hideAndShow: hideAndShowNavbar()
-    };
-    // on page end of charger the script execute this instructions
-    $(function () {
-        app.hideAndShow;
-    });
-    // functions
-    function hideAndShowNavbar() {
-        var prevScrollpos = window.pageYOffset;
-        window.onscroll = function () {
-            var currentScrollPos = window.pageYOffset;
-            if (prevScrollpos > currentScrollPos) {
-                document.getElementById("navbar").style.top = "0";
-            } else {
-                document.getElementById("navbar").style.top = "-64px";
-            }
-            prevScrollpos = currentScrollPos;
-        }
-    }
-}));
+$(document).ready(function(){
+	$('div[align=center]').css('display', 'none');
+});
